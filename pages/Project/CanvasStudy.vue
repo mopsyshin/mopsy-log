bd00339238d6e62da43131298c3db953469c85eb
mopsyshin <mopsyshin@gmail.com>
2019-01-24 18:29:34 +0900

    canvas refactoring

--- pages/Project/CanvasStudy.vue
+++ pages/Project/CanvasStudy.vue
@@ -22,10 +22,41 @@ export default {
   data () {
     return {
       canvasWidth: 0,
+      canvas: null,
+      ctx: null,
+      density: 50,
+      particles: [],
+      Particle: function(scale, color, vx, vy, gv, canvas, ctx) {
+        this.scale = scale;
+        this.color = color;
+        this.vx = vx;
+        this.vy = vy;
+        this.gv = gv;
+        this.position = {
+            x: 0,
+            y: 0
+        };
+        this.draw = function() {
+          ctx.beginPath();
+          ctx.arc(this.position.x, this.position.y, this.scale, 0, 2*Math.PI, false);
+          ctx.fillStyle = this.color;
+          ctx.fill();
+        };
+        this.update = function() {
+          this.vy += this.gv;
+          this.position.x += this.vx;
+          this.position.y += this.vy;
+          if (this.position.y > canvas.height - this.scale) {
+              this.vy *= -0.6;
+              this.vx *= 0.85;
+              this.position.y = canvas.height - this.scale;
+          }
+          this.draw();
+        };
+      },
     };
   },
   mounted() {
-    // canvas width setting 
     if (window.innerWidth > 768) {
       this.canvasWidth = 712;
     } else {
@@ -44,86 +75,40 @@ export default {
           };
       })();
 
-      const canvas = this.$refs.canvas;
-      const ctx = canvas.getContext('2d');
+      this.canvas = this.$refs.canvas;
+      this.ctx = this.canvas.getContext('2d');
 
-      // particle definition
-      let Particle = function(scale, color, vx, vy, gv) {
-          this.scale = scale;
-          this.color = color;
-          this.vx = vx;
-          this.vy = vy;
-          this.gv = gv;
-          this.position = {
-              x: 0,
-              y: 0
-          };
-      };
-
-      // particle draw function
-      Particle.prototype.draw = function() {
-          ctx.beginPath();
-          ctx.arc(this.position.x, this.position.y, this.scale, 0, 2*Math.PI, false);
-          ctx.fillStyle = this.color;
-          ctx.fill();
-      };
-
-      // particle update function
-      Particle.prototype.update = function() {
-          this.vy += this.gv;
-          this.position.x += this.vx;
-          this.position.y += this.vy;
-          if (this.position.y > canvas.height - this.scale) {
-              this.vy *= -0.6;
-              this.vx *= 0.85;
-              this.position.y = canvas.height - this.scale;
-          }
-          this.draw();
-      };
-
-      // declare number of particles
-      const density = 50; 
-      let particles = []; 
-
-      // generate particle, and make them as array
-      for (let i = 0; i < density; i++) {
-          // generate random particle properties
+      for (let i = 0; i < this.density; i++) {
           const r = ~~(Math.random()*255);
-          const g = toPositive(r - (~~(Math.random()*150)));
-          const b = toPositive(g - (~~(Math.random()*30)));
+          const g = this.toPositive(r - (~~(Math.random()*150)));
+          const b = this.toPositive(g - (~~(Math.random()*30)));
           const color = `rgb(${r},${g},${b})`;
           const scale = ~~(Math.random()*15)+3;
           const x = Math.random() * 10 - 5;
           const y = Math.random() * 9 + 4;
           const gravity = Math.random() * 0.2 + 0.3;
           
-          particles[i] = new Particle(scale, color, x, -y, gravity);
-          particles[i].position.x = canvas.width / 2;
-          particles[i].position.y = 200;
+          this.particles[i] = new this.Particle(scale, color, x, -y, gravity, this.canvas, this.ctx);
+          this.particles[i].position.x = this.canvas.width / 2;
+          this.particles[i].position.y = 200;
       }
-
-      function toPositive(v) {
-        if (v < 0) {
-          return -v;
-        }
-        return v;
-      }
-
-      loop();
-      // loop function
-      function loop() {
-          requestAnimFrame(loop);
-          // remove last frame
-          ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height);
-          for (var i in particles) {
-            // update particle state
-              particles[i].update();
-          }
-      }
+      this.loop();
     });
-    
   },
   methods: {
+    toPositive(v) {
+      if (v < 0) {
+        return -v;
+      }
+      return v;
+    },
+    loop() {
+      requestAnimFrame(this.loop);
+      this.ctx.clearRect(0,0, this.ctx.canvas.width, this.ctx.canvas.height);
+      for (var i in this.particles) {
+        this.particles[i].update();
+      }
+    },
   },
   components: {
 
